c99123814ed6af793cd2ec35cb93c7f2b20c8db9
Parimalsai Alapati <parimalsai.alapati@gmail.com>
2026-04-26 23:25:20 +0530

    updated scaler logic and added safety thresholds

--- scaler_logic.py
+++ scaler_logic.py
@@ -1,8 +1,13 @@
-def decide(rt):
-    if rt > 800:
+# scaler_logic.py
+
+def decide(rt, current_workers, max_workers, min_workers):
+    # Scale out if slow, BUT only if we haven't hit the max limit
+    if rt > 800 and current_workers < max_workers:
         return "scale_out"
-    elif rt < 300:
+        
+    # Scale in if fast, BUT only if we have more than the minimum
+    elif rt < 300 and current_workers > min_workers:
         return "scale_in"
+        
     else:
         return "stable"
-
